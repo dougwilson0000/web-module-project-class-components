af09f8706ff4141b0bbeffece75b63b0475af39c
Douglas Wilson <dougwilson0000@gmail.com>
2022-01-10 21:34:12 -0500

    Setup Dom, starting to make code modular

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import React from 'react';
+import Todo from './components/Todo';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+
+
+
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+
+   handleAdd = (input) => {
+    const newTodo = {
+      task: input,
+      id: Date.now(),
+      completed: false
+    } 
+    this.setState({
+       ...this.state,
+      todos: [...this.state.todos, newTodo]
+     });
+   }
+    handleClear = () => {
+      this.setState({
+        ...this.state,
+        todos: this.state.todos.filter(todo => {
+          return (todo.completed === false);
+        })
+      })
+
+    }
+  
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
+      const { todos } = this.state;
+      
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todos</h1>
+        <TodoList  todos={todos} /> 
+        <TodoForm handleAdd={this.handleAdd}/>
+        
+        <button onClick={this.handleClear}>Clear</button>
       </div>
     );
   }
---
+++ src/components/Todo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Todo extends React.Component {
+		render() {
+                return (<li key={this.props.todo.id}>{this.props.todo.task} { this.props.todo.completed ?<span> - completed</span> : <span></span> }</li>)
+}
+}
+
+export default Todo;
+
